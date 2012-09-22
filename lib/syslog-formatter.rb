d7b3db7112fb44c7931fe1ab6c791c1ff87dd56c
scambra <sergio@enpijama.es>
2012-09-21 22:43:37 -1000

    remove datetime, because is already added by syslog, and log severity like a tag from ActiveSupport::TaggedLogging

--- lib/syslog-formatter.rb
+++ lib/syslog-formatter.rb
@@ -3,7 +3,7 @@
 #   [Time.mSec] [SeverityLabel]: message
 
 class Logger::SyslogFormatter < Logger::Formatter
-  Format = "[%s] [%5s]: %s\n"
+  Format = "[%5s] %s\n"
 
   attr_accessor :datetime_format
 
@@ -12,18 +12,10 @@ class Logger::SyslogFormatter < Logger::Formatter
   end
 
   def call(severity, time, progname, msg)
-    Format % [format_datetime(time), severity, msg2str(msg)]
+    Format % [severity, msg2str(msg)]
   end
 
   protected
-
-  def format_datetime(time)
-    if @datetime_format.nil?
-      time.strftime("%H:%M:%S.") << "%06d " % time.usec
-    else
-      time.strftime(@datetime_format)
-    end
-  end
 
   def msg2str(msg)
     case msg
